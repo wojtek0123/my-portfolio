096428e687ba6f1db7a9be74fd5267d8ef3be3f0
wojtek <wojtekpietraszuk@gmail.com>
2022-04-16 17:00:00 +0200

    Update file

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,2 @@
-const burgerBtn = document.querySelector('.nav__burger');
-const mobileNav = document.querySelector('.nav__links-mobile');
-
-const burgerHandler = () => {
-  mobileNav.classList.toggle('show');
-  burgerBtn.classList.toggle('open');
-};
-
-burgerBtn.addEventListener('click', burgerHandler);
-mobileNav.addEventListener('click', (event) => {
-  if (event.target.classList.contains('nav__link-mobile')) {
-    burgerHandler();
-  }
-});
+import './auto-typing.js';
+import './hamburger.js';

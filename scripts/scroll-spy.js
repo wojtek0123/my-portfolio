18c112b1d90fdaf3a8da8bd92117b9c7692f4498
wojtek <wojtekpietraszuk@gmail.com>
2022-04-22 15:30:20 +0200

    Removed scroll spy function and added auto hide nav

--- scripts/scroll-spy.js
+++ scripts/scroll-spy.js
@@ -2,7 +2,21 @@ const headerArrowDown = document.querySelector('.header__arrow');
 const sections = [...document.querySelectorAll('section')];
 const desktopNavLinks = [...document.querySelectorAll('.nav__circle')];
 
+const nav = document.querySelector('.nav');
+
 let currentSection = 0;
+let prevScrollPos = window.screenY;
+
+const hideNav = () => {
+	currentSection = window.scrollY;
+
+	if (currentSection > prevScrollPos) {
+		nav.classList.add('hide');
+	} else {
+		nav.classList.remove('hide');
+	}
+	prevScrollPos = currentSection;
+};
 
 const hideArrow = () => {
 	currentSection = window.scrollY;
@@ -13,36 +27,35 @@ const hideArrow = () => {
 	}
 };
 
-const scrollSpyHandler = () => {
-	currentSection = window.scrollY;
+// const scrollSpyHandler = () => {
+// 	currentSection = window.scrollY;
 
-	if (currentSection === 0) {
-		desktopNavLinks[0].classList.add('active');
-	}
+// 	if (currentSection === 0) {
+// 		desktopNavLinks[0].classList.add('active');
+// 	}
 
-	sections.forEach((section, index) => {
-		if (section.offsetTop <= currentSection + 200) {
-			desktopNavLinks[index + 1].classList.add('active');
-		} else if (section.offsetTop > currentSection) {
-			desktopNavLinks[index + 1].classList.remove('active');
-		}
-	});
+// 	sections.forEach((section, index) => {
+// 		if (section.offsetTop <= currentSection + 200) {
+// 			desktopNavLinks[index + 1].classList.add('active');
+// 		} else if (section.offsetTop > currentSection) {
+// 			desktopNavLinks[index + 1].classList.remove('active');
+// 		}
+// 	});
 
-	if (currentSection >= document.body.offsetHeight - window.innerHeight) {
-		desktopNavLinks[desktopNavLinks.length - 1].classList.add('active');
-	}
-};
+// 	if (currentSection >= document.body.offsetHeight - window.innerHeight) {
+// 		desktopNavLinks[desktopNavLinks.length - 1].classList.add('active');
+// 	}
+// };
 
-const checkScrollSpy = () => {
-	if (currentSection === 0) {
-		desktopNavLinks[0].classList.add('active');
-	}
-};
+// const checkScrollSpy = () => {
+// 	if (currentSection === 0) {
+// 		desktopNavLinks[0].classList.add('active');
+// 	}
+// };
 
 const scrollHandler = () => {
 	hideArrow();
-	scrollSpyHandler();
-}
+	hideNav();
+};
 
 window.addEventListener('scroll', scrollHandler);
-window.addEventListener('DOMContentLoaded', checkScrollSpy);

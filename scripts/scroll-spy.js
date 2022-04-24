1e65deec53c93e6c9fd3af6340b55fdcaa01fe78
wojtek <wojtekpietraszuk@gmail.com>
2022-04-24 10:50:55 +0200

    Change currentSection to currentScrollPos

--- scripts/scroll-spy.js
+++ scripts/scroll-spy.js
@@ -4,54 +4,28 @@ const desktopNavLinks = [...document.querySelectorAll('.nav__circle')];
 
 const nav = document.querySelector('.nav');
 
-let currentSection = 0;
+let currentScrollPos = 0;
 let prevScrollPos = window.screenY;
 
 const hideNav = () => {
-	currentSection = window.scrollY;
+	currentScrollPos = window.scrollY;
 
-	if (currentSection > prevScrollPos) {
+	if (currentScrollPos > prevScrollPos) {
 		nav.classList.add('hide');
 	} else {
 		nav.classList.remove('hide');
 	}
-	prevScrollPos = currentSection;
+	prevScrollPos = currentScrollPos;
 };
 
 const hideArrow = () => {
-	currentSection = window.scrollY;
-	if (currentSection < 200) {
+	currentScrollPos = window.scrollY;
+	if (currentScrollPos < 200) {
 		headerArrowDown.classList.remove('hide');
 	} else {
 		headerArrowDown.classList.add('hide');
 	}
 };
-
-// const scrollSpyHandler = () => {
-// 	currentSection = window.scrollY;
-
-// 	if (currentSection === 0) {
-// 		desktopNavLinks[0].classList.add('active');
-// 	}
-
-// 	sections.forEach((section, index) => {
-// 		if (section.offsetTop <= currentSection + 200) {
-// 			desktopNavLinks[index + 1].classList.add('active');
-// 		} else if (section.offsetTop > currentSection) {
-// 			desktopNavLinks[index + 1].classList.remove('active');
-// 		}
-// 	});
-
-// 	if (currentSection >= document.body.offsetHeight - window.innerHeight) {
-// 		desktopNavLinks[desktopNavLinks.length - 1].classList.add('active');
-// 	}
-// };
-
-// const checkScrollSpy = () => {
-// 	if (currentSection === 0) {
-// 		desktopNavLinks[0].classList.add('active');
-// 	}
-// };
 
 const scrollHandler = () => {
 	hideArrow();
